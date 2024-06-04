5fe9b45a111cb4abf16646fb4189bc0f13c415f3
Sneha Chandrika Anil <161577875+Sneha-Chandrika-Anil@users.noreply.github.com>
2024-06-04 18:22:18 +0530

    Create Array_changeElement.java

---
+++ Array_changeElement.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+    String[] cricketers = {"Virat", "Dhoni", "Sachin",       "Bumrah"};
+    cricketers[1] = "Zaheer";
+    System.out.println(cricketers[1]);
+  }
+}
+​
