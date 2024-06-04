fb6b7dc264a41a42ceb172d75d1548e9a6fc06c6
Sneha Chandrika Anil <161577875+Sneha-Chandrika-Anil@users.noreply.github.com>
2024-06-04 18:26:44 +0530

    Create LoopThruArray.java

---
+++ LoopThruArray.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    String[] bts = {"Jin", "V", "Jungkook", "Suga"};
+    for (int i = 0; i < bts.length; i++) {
+      System.out.println(bts[i]);
+    }
+  }
+}
+​
